7de2b47021a18cd54401975b59129e858b653feb
yufeng <jeff.yu@ndpmedia.com>
2015-04-14 22:54:48 -0700

    add logging in constructor

--- ext/constructor.py
+++ ext/constructor.py
@@ -7,8 +7,6 @@ from config.setting import *
 from ext.util import *
 
 
-
-
 class Constructor(object):
 
     def __init__(self):
@@ -23,15 +21,22 @@ class Constructor(object):
                 offer_id, aff_id = data
                 t = string.Template(click_url_template)
                 click_url = t.substitute({"offerid":offer_id, "affid":aff_id})
-                r = requests.get(click_url, allow_redirects=False)
-                jumpaddress = r.text
-                transaction_id = get_clickid(jumpaddress)
-                t = string.Template(conv_url_template)
-                conv_url = t.substitute({"transactionid":transaction_id})
-                r = requests.get(conv_url)
-                if r.text == 'success=true;conversioned':
-                    self.logger.info('data structure success:{0}'.format(transaction_id))
-                    self.transactionidList.append(transaction_id)
+                try:
+                    r = requests.get(click_url, allow_redirects=False)
+                except Exception as ex:
+                    self.logger.error(ex)
+                else:
+                    jumpaddress = r.text
+                    transaction_id = get_clickid(jumpaddress)
+                    t = string.Template(conv_url_template)
+                    conv_url = t.substitute({"transactionid":transaction_id})
+                    r = requests.get(conv_url)
+                    if r.text == 'success=true;conversioned':
+                        self.logger.info('data structure success:{0}'.format(transaction_id))
+                        self.transactionidList.append(transaction_id)
+                    else:
+                        self.logger.error('{0} transaction_id:{1}'.format(r.text, transaction_id))
+
     @property
     def getTranasctionId(self):
         self.setClickConv
