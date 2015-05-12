fcbbcea9ec5e9181fe8e022436126fcd95570b24
yfsuse <yfsuse@gmail.com>
2015-05-12 18:37:25 +0800

    fix one bug that expect detail data count should equal to len(off_aff_list) * cycletime * 2

--- ext/util.py
+++ ext/util.py
@@ -51,7 +51,7 @@ def getDruidDetailResult(start_time, end_time, transaction_id_list):
         data = json.loads(r.text).get('data').get('data')
         logger.info('druid detail data count: {0}'.format(len(data[1:])))
         logger.info('expect detail data count: {0}'.format(len(offer_aff_combination) * cycletimes * 2 ))
-        if len(data[1:]) == len(offer_aff_combination) * (cycletimes+1):
+        if len(data[1:]) == len(offer_aff_combination) * (cycletimes*2):
             logger.info('get detail result success, druid detail data count equal to (offer_aff_combination * cycletimes)')
         else:
             logger.error('get detail result failed, druid detail data count not equal to (offer_aff_combination * cycletimes)')
