dd5235e52cb577f68a1b5ce57639a4e26046891a
yfsuse <yfsuse@gmail.com>
2015-05-12 17:17:24 +0800

    add logging

--- ext/util.py
+++ ext/util.py
@@ -45,6 +45,7 @@ def getDruidDetailResult(start_time, end_time, transaction_id_list):
     geturl = query_url + param
     logger.info('detail query: {0}'.format(geturl))
     time.sleep(timewaitquerydetail)
+    logging.info('wait {0} seconds that flow data to druid'.format(timewaitquerydetail))
     try:
         r = requests.get(geturl)
         data = json.loads(r.text).get('data').get('data')
