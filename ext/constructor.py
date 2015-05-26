24bbd0e9ab8fc6a0f0d9e3aa13f6311e0f355671
yufeng <jeff.yu@ndpmedia.com>
2015-05-26 16:01:10 +0800

    split setclickconv method to setClick and setConf method
    add time.sleep() when send conv request

--- ext/constructor.py
+++ ext/constructor.py
@@ -17,11 +17,12 @@ class Constructor(object):
 
     def __init__(self):
         self.transactionidList = list()
-        self.datacount = 0
+        self.clickdatacount = 0
+        self.convdatacount = 0
         self.logger = getLogger()
 
     @property
-    def setClickConv(self):
+    def setClick(self):
         for runtime in range(cycletimes):
             for data in offer_aff_combination:
                 offer_id, aff_id = data
@@ -33,36 +34,43 @@ class Constructor(object):
                         self.logger.error('send click data get a error response code: [{0}]'.format(r.status_code))
                         sys.exit()
                     else:
-                        self.datacount += 1
-                        self.logger.info('imitate click data success')
+                        self.clickdatacount += 1
+                        self.logger.info('set click data success')
                 except Exception as ex:
                     traceback.print_exc()
                 else:
                     jumpaddress = r.text
                     transaction_id = get_clickid(jumpaddress)
                     self.transactionidList.append(transaction_id)
-                    t = string.Template(conv_url_template)
-                    conv_url = t.substitute({"transactionid":transaction_id})
-                    time.sleep(randint(1,3))
-                    if randint(2,2) % 2 == 0:  # 100% percent to mock conversion data
-                        try:
-                            r = requests.get(conv_url)
-                            if r.status_code == 200:
-                                self.datacount += 1
-                                self.logger.info('imitate conversion data success')
-                            else:
-                                pass
-                        except Exception as ex:
-                            self.logger.error('set conv data failed')
+        self.logger.info('set click data count: {0}'.format(self.clickdatacount))
+
+    @property
+    def setConv(self):
+        for tid in self.transactionidList:
+            t = string.Template(conv_url_template)
+            conv_url = t.substitute({"transactionid":tid})
+            if randint(2,2) % 2 == 0:  # 100% percent to mock conversion data
+                try:
+                    r = requests.get(conv_url)
+                    if r.text != "success=true;conversioned":
+                        self.logger.error(r.text)
                     else:
-                        pass
+                        self.convdatacount += 1
+                        self.logger.info('set conv data success')
+                except Exception as ex:
+                    traceback.print_exc()
+        self.logger.info('set click data count: {0}'.format(self.convdatacount))
 
     @property
     def getTranasctionId(self):
         self.logger.info('set click and conversion data begin')
-        self.setClickConv
+        self.setClick
+        time.sleep(10)
+        self.setConv
         self.logger.info('set click and conversion data end')
-        return self.datacount, self.transactionidList
+        datacount = add(self.clickdatacount, self.convdatacount)
+        return datacount, self.transactionidList
 
 if __name__ == '__main__':
-    pass
+    c = Constructor()
+    c.getTranasctionId
